1689c03bebaef387331f9dd864ef1c8dcada8aa1
Michael <soplomiha@gmail.com>
2022-06-09 00:15:36 +0300

    Upgrade the bot to a stable version

--- test_bot.py
+++ test_bot.py
@@ -1,16 +1,13 @@
-import asyncio
 import os
 
-import telegram
 from dotenv import load_dotenv
+import telegram
 
-
-async def main(token):
-    bot = telegram.Bot(token)
-    async with bot:
-        await bot.send_message(text='Hi John!', chat_id=1724291408)
 
 if __name__ == '__main__':
     load_dotenv()
-    tg_token = os.getenv("TG_BOT_TOKEN")
-    asyncio.run(main(tg_token))
+    tg_token = os.getenv('TG_BOT_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
+
+    bot = telegram.Bot(token=tg_token)
+    bot.send_message(chat_id=tg_chat_id, text="Привет, Михаил!")
