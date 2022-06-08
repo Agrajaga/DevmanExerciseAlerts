3fbb07c6fd164ac755bd3fc71930355526bed8c3
Michael <soplomiha@gmail.com>
2022-06-09 00:52:14 +0300

    Add sending messages to telegram

--- test_api.py
+++ test_api.py
@@ -1,12 +1,16 @@
 import os
-from dotenv import load_dotenv
-import requests
-import pprint
 
+import requests
+import telegram
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
     load_dotenv()
     devman_token = os.getenv("DEVMAN_API_TOKEN")
+    tg_token = os.getenv('TG_BOT_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
+
+    bot = telegram.Bot(token=tg_token)
 
     headers = {
         "Authorization": devman_token,
@@ -17,20 +21,32 @@ if __name__ == "__main__":
     while True:
         try:
             response = requests.get(
-                "https://dvmn.org/api/long_polling/", 
+                "https://dvmn.org/api/long_polling/",
                 headers=headers,
-                timeout=timeout, 
+                timeout=timeout,
                 params=params)
             response.raise_for_status()
             answer = response.json()
 
-            pprint.pprint(answer)
             params = {}
             if answer["status"] == "timeout":
                 params = {
                     "timestamp": answer["timestamp_to_request"],
                 }
+            if answer["status"] == "found":
+                for attempt in answer["new_attempts"]:
+                    work_status = "Преподавателю все понравилось, можно приступать к следующему уроку!"
+                    if attempt["is_negative"]:
+                        work_status = "К сожалению в работе нашлись ошибки\."
+                    work_desc = f'["{attempt["lesson_title"]}"]({attempt["lesson_url"]})'
+                    message = f'Преподаватель проверил работу {work_desc}\. {work_status}'
+                    bot.send_message(
+                        chat_id=tg_chat_id,
+                        text=message,
+                        parse_mode=telegram.ParseMode.MARKDOWN_V2,
+                        disable_web_page_preview=True,
+                    )
         except requests.exceptions.ReadTimeout:
-            print("Timeout")
+            print("No answer from server.")
         except requests.exceptions.ConnectionError:
-            pass
+            print("Connection error. Reconnecting...")
